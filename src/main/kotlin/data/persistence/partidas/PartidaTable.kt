22bffc43f4633d1de6144f5c4bc04cca56a57e0b
JulioMoreno <juliomorenomadueno@gmail.com>
2025-02-15 23:15:28 +0100

    Contenedor Dokcer/ SQL

--- src/main/kotlin/data/persistence/partidas/PartidaTable.kt
+++ src/main/kotlin/data/persistence/partidas/PartidaTable.kt
@@ -6,6 +6,6 @@ object PartidaTable: IntIdTable("Partida") {
     val nombrePartida = varchar("nombre", 50).uniqueIndex()
     val resultado = varchar("resultado", 20)
     val estadistica = varchar("estadistica", 5)
-    val fecha = varchar("fecha", 10)
+    val fecha = varchar("fecha", 12)
     val token = varchar("token", 255).nullable()
 }
