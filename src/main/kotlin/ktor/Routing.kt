37cf93c30932befe3a08fc145e674ebfe3e2dbb2
JulioMoreno <juliomorenomadueno@gmail.com>
2025-02-15 17:58:37 +0100

    Inicio de Routing (Get generico con Queris)

--- src/main/kotlin/ktor/Routing.kt
+++ src/main/kotlin/ktor/Routing.kt
@@ -1,5 +1,8 @@
 package com.example.ktor
 
+import domain.models.partidas.Resultado
+import domain.usecase.UseCaseProvider
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.response.*
@@ -7,9 +10,44 @@ import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
+
+        //EndPoint de ejemplo
         get("/") {
             call.respondText("Hello World!")
         }
+
+
+        get("/partida") {
+            val nombrePartida = call.request.queryParameters["nombre"]
+
+            if (nombrePartida != null){
+                val partida = UseCaseProvider.getPartidasByNombre(nombrePartida)
+                if (partida == null){
+                    call.respond(HttpStatusCode.NotFound, "Empleado no encontrado")
+                }else{
+                    call.respond(partida)
+                }
+
+                return@get
+            }
+
+            val resultado = call.request.queryParameters["resultado"]
+
+            if (resultado != null){
+                try{
+                    val resul = resultado.uppercase()
+                    val partidas = UseCaseProvider.getPartidasByResultado(Resultado.valueOf(resultado.uppercase()))
+                    call.respond(partidas)
+                }catch (e: IllegalArgumentException){
+                    call.respond(HttpStatusCode.BadRequest, "El resultado no es valido")
+                }
+            }else{
+                val partidas = UseCaseProvider.getAllPartidas()
+                call.respond(partidas)
+            }
+        }
+
+
         // Static plugin. Try to access `/static/index.html`
         staticResources("/static", "static")
     }
